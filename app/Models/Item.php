6d5575f54ebd36a9bbba22d8def2313b63449144
Paul Novack <paul.novack@gmail.com>
2026-07-05 12:29:39 -0500

    adds @property-read Box $box to Item model for PHPStan

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property-read \App\Models\Box $box
+ */
 class Item extends Model
 {
     /** @use HasFactory<ItemFactory> */
